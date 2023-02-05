ebaf28d20dfad53ae55ea31c0dd414e4284f92dd
jsanchezdepablo <jsdp93@gmail.com>
2023-02-05 13:43:31 +0100

    Added filter function

--- src/scenes/podcasts/index.jsx
+++ src/scenes/podcasts/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import TextField from "@mui/material/TextField";
 import { usePodcastsSelector, PodcastsActions } from "states/podcasts";
 import DefaultCircularProgress from "components/circular-progress";
@@ -7,6 +7,9 @@ import Card from "./components/card";
 import "./styles.scss";
 
 const PodcastsView = () => {
+  const [filterValue, setFilterValue] = useState(null);
+  const [filteredPodcasts, setFilteredPodcasts] = useState([]);
+
   const {
     state: { podcasts, isLoading },
   } = usePodcastsSelector();
@@ -17,17 +20,38 @@ const PodcastsView = () => {
     searchPodcasts();
   }, []);
 
-  const podcastCards = useMemo(() => {
+  useEffect(() => {
     if (podcasts.length > 0) {
-      return podcasts.map((podcast) => <Card key={podcast.title} data={podcast} />);
+      if (filterValue !== null) {
+        const q = filterValue?.toLowerCase();
+        const filtered = podcasts.filter(
+          ({ title, artist }) => title.toLowerCase()?.includes(q) || artist.toLowerCase()?.includes(q),
+        );
+        setFilteredPodcasts([...filtered]);
+      } else {
+        setFilteredPodcasts([...podcasts]);
+      }
     }
-  }, [podcasts]);
+  }, [filterValue, podcasts]);
+
+  const podcastCards = useMemo(() => {
+    if (filteredPodcasts.length > 0) {
+      return filteredPodcasts.map((podcast) => <Card key={podcast.title} data={podcast} />);
+    }
+  }, [filteredPodcasts]);
 
   return (
     <div>
       <div className="podcast__filter-container">
-        <div className="podcast__filter-count">{podcasts.length}</div>
-        <TextField label="Filter podcasts" variant="outlined" size="small" />
+        <div className="podcast__filter-count">{filteredPodcasts.length}</div>
+        <TextField
+          label="Filter podcasts"
+          variant="outlined"
+          size="small"
+          onChange={(e) => {
+            setFilterValue(e.target.value);
+          }}
+        />
       </div>
       {isLoading ? <DefaultCircularProgress /> : <div className="podcast__cards">{podcastCards}</div>}
     </div>
