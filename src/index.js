66e74c404da581f27afc57a5222a8e453cf2b97d
jsanchezdepablo <jsdp93@gmail.com>
2023-02-05 10:51:59 +0100

    Added some extra configuration to the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import GlobalStateProvider from "states";
 import Layout from "layout";
 import routes from "routes";
 
-import "./index.css";
+import "assets/styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const Application = () => (
@@ -15,8 +14,3 @@ const Application = () => (
 );
 
 root.render(<Application />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
