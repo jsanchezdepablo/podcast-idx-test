52bad528df18dd0ae88cbbad03b526143d260a92
jsanchezdepablo <jsdp93@gmail.com>
2023-02-05 20:05:32 +0100

    Added new LargeCard component

---
+++ src/scenes/podcasts/components/large-card/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./styles.scss";
+
+const LargeCard = ({ data }) => (
+  <div className="large-card">
+    <div className="large-card__image">
+      <img src={data?.image} />
+    </div>
+
+    <div>
+      <hr />
+    </div>
+
+    <div className="large-card__primary-text">
+      <div className="large-card__primary-text--title">{data?.title}</div>
+      <div className="large-card__primary-text--subtitle">by {data?.artist}</div>
+    </div>
+
+    <div>
+      <hr />
+    </div>
+
+    <div className="large-card__secondary-text">
+      <div className="large-card__secondary-text--title">Description:</div>
+      <div className="large-card__secondary-text--subtitle">by {data?.description}</div>
+    </div>
+  </div>
+);
+
+export default LargeCard;
