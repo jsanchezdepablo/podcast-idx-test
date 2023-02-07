9639cb1980670666ed5c540d47cb730097249460
Jesús Sánchez de Pablo <jsdp93@gmail.com>
2023-02-07 18:44:27 +0100

    Revert "Updated version to 1.0.0"
    This reverts commit 52ac24e9dc5d061cef4431c34d6ebc3f23f929c0.

--- src/states/podcasts/index.js
+++
@@ -1,6 +0,0 @@
-import { Provider, Selector } from "./context";
-import Actions from "./actions";
-
-export const PodcastsProvider = Provider;
-export const usePodcastsSelector = Selector;
-export const PodcastsActions = Actions;
